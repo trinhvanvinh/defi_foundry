9291d2477d83a24ce7dcde062c3fde19a982b758
vinhtrinhvan <trinhvanvinh018>
2023-08-05 17:34:25 +0700

    Pair factory

---
+++ src/uniswap-v2-core/UniswapV2Factory.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "./interfaces/IUniswapV2Factory.sol";
+import "./UniswapV2Pair.sol";
+
+contract UniswapV2Factory is IUniswapV2Factory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(
+        address tokenA,
+        address tokenB
+    ) external returns (address pair) {
+        require(tokenA != tokenB, "Uniswapv2: Identical address");
+        (address token0, address token1) = tokenA < tokenB
+            ? (tokenA, tokenB)
+            : (tokenB, tokenA);
+        require(token0 != address(0), "Uniswapv2: Zero address");
+        require(getPair[token0][token1] == address(0), "Uniswapv2: Pair_exsts");
+        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IUniswapV2Pair(pair).initialize(token0, token1);
+        getPair[tokenA][tokenB] = pair;
+        getPair[tokenB][tokenA] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, "UniswapV2: Forbidden");
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, "Uniswapv2: Forbidden");
+        feeToSetter = _feeToSetter;
+    }
+}
